b4cd5da2d8c0227eacb476334c394dec34a37d1d
Alex Ishchenko <101579409+Act1on04@users.noreply.github.com>
2023-10-22 19:52:39 +0300

    Init

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    kotlin("jvm") version "1.8.0"
+    application
+}
+
+group = "de.hs-anhalt"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.junit.jupiter:junit-jupiter:5.10.0")
+//    implementation("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+//    implementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
+//    implementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(11)
+}
+
+application {
+    mainClass.set("MainKt")
+}
