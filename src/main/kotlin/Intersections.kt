f8ac9403ed5355b0d87200bc57de30c727ae49ae
Act1on04 <101579409+Act1on04@users.noreply.github.com>
2023-12-14 17:55:19 +0200

    Update

--- src/main/kotlin/Intersections.kt
+++ src/main/kotlin/Intersections.kt
@@ -1,14 +1,13 @@
 class Intersections(vararg intersections: Intersection) {
-    private val intersections = intersections.sortedBy { it.t }
+    private var intersections = intersections.sortedBy { it.t }
 
     val count: Int
         get() = intersections.size
 
     fun hit(): Intersection? = intersections.filter { it.t > 0.0 }.minByOrNull { it.t }
+//    fun hit(): Intersection? = intersections.firstOrNull { intersection -> intersection.t > 0 }
 
-    operator fun get(index: Int): Intersection {
-        return intersections[index]
-    }
+    operator fun get(index: Int): Intersection = intersections[index]
 
 
 }
--- src/main/kotlin/Scene.kt
+++ src/main/kotlin/Scene.kt
@@ -12,6 +12,37 @@ class Scene {
         return objects
     }
 
+//    fun traceRay(ray: Ray): Intersections {
+//        val intersections = objects.map { it.intersect(ray) }
+//        return intersections
+//    }
+
+
+//    internal fun intersect(ray: Ray): Intersections =
+//        shapes.flatMap { it.intersect(ray) }.sortedBy { it.t }
+//
+//    fun intersect(ray: Ray): Intersections {
+//        val xss = objects.map { it.intersect(ray) }
+//        return Intersections.combine(xss)
+//    }
+//
+//    fun combine(intersections: List<Intersections>): Intersections {
+//        val all = intersections.flatMap { it.intersections.asList() }
+//        val sorted = all.sortedBy { it.t }
+//        return Intersections(*sorted.toTypedArray())
+//    }
+//
+    fun traceRay(ray: Ray): Intersections {
+
+        // Перебираем все объекты на сцене и вычисляем их пересечения с лучом
+        val objectsIntersection = objects.map { it.intersect(ray) }
+        val allIntersections = objectsIntersection.flatMap { it.intersections.asList() }
+        val sorted = allIntersections.sortedBy { it.t }
+        return Intersections(*sorted.toTypedArray())
+
+    }
+
+
     // Статический метод для создания тестовой сцены
     companion object {
         fun defaultScene(): Scene {
